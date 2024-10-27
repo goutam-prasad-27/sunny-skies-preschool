f5c5ec251a05bd9fd1a939e3f86fceda97e1ef5a
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-10-27 10:54:59 +0530

    hoc added

--- src/HOC/SectionWrapper.jsx
+++ src/HOC/SectionWrapper.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { styles } from "../styles";
 
 const SectionWrapper = (Component, idName) =>
 	function HOC() {
 		return (
-			<section className="">
-				<span className="" id={idName}></span>
+			<section
+				className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+			>
+				<span className="hash-span" id={idName}></span>
 				<Component />
 			</section>
 		);
