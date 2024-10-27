a688e95fae0e6039759f1144e7f0002329472a2f
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-10-27 12:47:37 +0530

    new images added for admission section

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -9,6 +9,13 @@ import napTime from "../Assets/galleryImg/nap_time.jpg";
 import blockTime from "../Assets/galleryImg/block_time.mp4";
 import interactTime from "../Assets/galleryImg/interact_time.mp4";
 
+import firstDay from "../Assets/admissionImg/first_day.jpg";
+import applyDay from "../Assets/admissionImg/apply_day.jpg";
+import tourDay from "../Assets/admissionImg/tour_day.jpg";
+import interviewDay from "../Assets/admissionImg/interview_day.jpg";
+import orientationDay from "../Assets/admissionImg/orientation_day.jpg";
+import acceptanceDay from "../Assets/admissionImg/acceptance_day.jpg";
+
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -25,6 +32,12 @@ export {
 	toyTime,
 	artTime,
 	playTime,
+	firstDay,
+	applyDay,
+	tourDay,
+	interviewDay,
+	orientationDay,
+	acceptanceDay,
 	menu,
 	close,
 	FaFacebook,
