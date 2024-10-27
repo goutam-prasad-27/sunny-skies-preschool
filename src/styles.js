c2e4f15a919adf7e8c0757a7d8b2d0eba0cb2bcf
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-10-27 10:57:31 +0530

    section style added

--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,12 @@
 export const styles = {
 	paddingX: "px-6 sm:px-10",
 	paddingY: "py-6 sm:py-10",
-	padding: "px-[30px] sm:px-[100px] py-[30px] sm:py-[90px]",
+	padding: "px-[30px] sm:px-[50px] py-[30px] sm:py-[50px]",
 
-	heroButton: "font-bold border-2 px-5 py-3 rounded-3xl tracking-wider hover:underline hover:underline-offset-2",
+	heroButton:
+		"font-bold border-2 px-5 py-3 rounded-3xl tracking-wider hover:underline hover:underline-offset-2",
 
-	sectionHeading: "",
-	sectionSubText: "",
+	sectionHeading: "pb-5 text-5xl font-bold tracking-wide",
+	sectionSubText: "pl-3 text-lg leading-6 font-medium",
+	sectionSubHeading: "text-4xl font-semibold tracking-wide pb-5",
 };
