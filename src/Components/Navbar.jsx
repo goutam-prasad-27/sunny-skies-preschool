242cdd0efda3befd959c3bf0c1d67f9e123130a6
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-10-27 10:56:24 +0530

    adjusted for sm device

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,90 +12,94 @@ function Navbar() {
 	const [toggle, setToggle] = useState(false); // 'toggle' controls the visibility of the mobile menu
 
 	return (
-		<nav
-			// Apply styles to the navigation bar
-			className={`${styles.paddingX} fixed w-full flex items-center py-2 top-0 z-20 bg-background`} //! bg-might change here
-		>
-			<div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-				<Link
-					// Link to the home page
-					to="/"
-					className="flex items-center gap-2 "
-					onClick={() => {
-						setActive("");
-						window.scrollTo(0, 0);
-					}}
-				>
-					<img
-						// Logo image
-						src={logo}
-						alt="logo"
-						className="w-10 h-10 object-contain"
-					/>
-					<p className="text-[16px] flex font-semiBold tracking-wide uppercase sm:text-[20px]">
-						Sunny Skies
-						<span className="sm:block hidden">
-							&#160;Preschool
-						</span>
-					</p>
-				</Link>
-
-				<ul className="hidden sm:flex flex-row gap-6 list-none">
-					{NavLinks.map((links) => (
-						<li
-							// Navigation link item
-							key={links.id}
-							className={`${
-								active === links.title
-									? "text-white"
-									: "text-red"
-							} text-[14px] cursor-pointer font-bold tracking-wider hover:underline hover:underline-offset-2`}
-							onClick={() => setActive(links.title)}
-						>
-							<a href={`#${links.id}`}>{links.title}</a>
-						</li>
-					))}
-				</ul>
-
-				<div className="sm:hidden flex flex-1 justify-end items-center">
-					<img
-						// Menu toggle button
-						src={toggle ? close : menu} //! icons will change here
-						alt=""
-						className="w-[20px] h-[20px] object-contain cursor-pointer"
-						onClick={() => setToggle(!toggle)}
-					/>
-					<div
-						// Mobile menu
-						className={`${
-							!toggle ? "hidden" : "flex"
-						} p-5 absolute top-10 right-0 mx-5 my-1 min-w[140px] z-10 rounded-2xl bg-black`} //! bg-will change here
+		<section className="flex justify-center items-center">
+			<nav
+				// Apply styles to the navigation bar
+				className={`${styles.paddingX} fixed sm:w-[950px] w-full flex items-center py-2 sm:top-5 top-0 z-20 bg-background drop-shadow-2xl sm:rounded-3xl rounded-none`} //! bg-might change here
+			>
+				<div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+					<Link
+						// Link to the home page
+						to="/"
+						className="flex items-center gap-2 "
+						onClick={() => {
+							setActive("");
+							window.scrollTo(0, 0);
+						}}
 					>
-						<ul className="list-none flex justify-end items-start flex-col gap-3">
-							{NavLinks.map((links) => (
-								<li
-									// Mobile menu link item
-									key={links.id}
-									className={`${
-										active === links.title
-											? "text-white"
-											: "text-red"
-									} text-[14px] font-bold tracking-wider cursor-pointer hover:underline hover:underline-offset-2`}
-									onClick={() => {
-										setToggle(!toggle);
-										setActive(links.title);
-									}}
-								>
-									<a href={`#${links.id}`}>
-										{links.title}
-									</a>
-								</li>
-							))}
-						</ul>
+						<img
+							// Logo image
+							src={logo}
+							alt="logo"
+							className="w-10 h-10 object-contain"
+						/>
+						<p className="text-[16px] flex font-semiBold tracking-wide uppercase sm:text-[20px]">
+							Sunny Skies
+							<span className="sm:block hidden">
+								&#160;Preschool
+							</span>
+						</p>
+					</Link>
+
+					<ul className="hidden sm:flex flex-row gap-6 list-none">
+						{NavLinks.map((links) => (
+							<li
+								// Navigation link item
+								key={links.id}
+								className={`${
+									active === links.title
+										? "text-white"
+										: "text-red"
+								} text-[14px] cursor-pointer font-bold tracking-wider hover:underline hover:underline-offset-2`}
+								onClick={() => setActive(links.title)}
+							>
+								<a href={`#${links.id}`}>
+									{links.title}
+								</a>
+							</li>
+						))}
+					</ul>
+
+					<div className="sm:hidden flex flex-1 justify-end items-center">
+						<img
+							// Menu toggle button
+							src={toggle ? close : menu} //! icons will change here
+							alt=""
+							className="w-[20px] h-[20px] object-contain cursor-pointer"
+							onClick={() => setToggle(!toggle)}
+						/>
+						<div
+							// Mobile menu
+							className={`${
+								!toggle ? "hidden" : "flex"
+							} p-5 absolute top-10 right-0 mx-5 my-1 min-w[140px] z-10 rounded-2xl bg-black`} //! bg-will change here
+						>
+							<ul className="list-none flex justify-end items-start flex-col gap-3">
+								{NavLinks.map((links) => (
+									<li
+										// Mobile menu link item
+										key={links.id}
+										className={`${
+											active === links.title
+												? "text-white"
+												: "text-red"
+										} text-[14px] font-bold tracking-wider cursor-pointer hover:underline hover:underline-offset-2`}
+										onClick={() => {
+											setToggle(!toggle);
+											setActive(links.title);
+										}}
+									>
+										<a href={`#${links.id}`}>
+											{links.title}
+										</a>
+									</li>
+								))}
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</section>
 	);
 }
 
