368ce5ef11d57192b4ae1ed61cdb2d5328fa79cf
goutam-prasad-27 <goutam.prasad.2707@gmail.com>
2024-11-02 23:16:32 +0530

    gallery section added

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { SectionWrapper } from "../HOC";
+import { styles } from "../styles";
+import { GalleryText, GalleryImages } from "../Constant";
+
+const GalleryCollection = ({ id, src }) => {
+	const isVideo = src.endsWith(".mp4");
+	return (
+		<div>
+			{isVideo ? (
+				<video
+					className="sm:h-[200px] h-[150px] rounded-2xl"
+					autoPlay
+					muted
+					loop
+				>
+					<source src={src} type="video/mp4" autoPlay />
+				</video>
+			) : (
+				<img
+					src={src}
+					alt={`Image ${id}`}
+					className="sm:h-[200px] rounded-2xl h-[150px]"
+				/>
+			)}
+		</div>
+	);
+};
 
 function Gallery() {
-	return <div>Gallery</div>;
+	return (
+		<section>
+			<div>
+				<h1 className={`${styles.sectionHeading}`}>Gallery.</h1>
+				<p className={`${styles.sectionSubText}`}>{GalleryText}</p>
+			</div>
+			<div className="my-5 h-[2px] bg-white" />
+			<div className="flex flex-wrap gap-3 items-center justify-center">
+				{GalleryImages.map((gallery, idx) => (
+					<GalleryCollection
+						key={gallery.id}
+						index={idx}
+						{...gallery}
+					/>
+				))}
+			</div>
+		</section>
+	);
 }
 
-export default Gallery;
+export default SectionWrapper(Gallery, "gallery");
